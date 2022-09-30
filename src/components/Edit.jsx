d88330b645ab8f3f34d78461cb6a21ccf62b06e7
Tushar7567 <swain.tushar00@gmail.com>
2022-09-30 15:56:36 +0530

    Update Edit.jsx

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,23 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 
-export default class Edit extends Component {
-  render() {
+const  Edit = (props) => {
+
+  
     return (
       <div>
-        <div className='edit-container'>
-            <div className='edit-row'>
-                <input type="text" autoComplete='true' placeholder='Name'/>
-                <input type="number" autoComplete='true' placeholder='Age'/>
+        {
+          props.sArr.map((item, index) => {
+            return <div key={`${index}`}>
+              <div className='edit-container'>
+                <div className='edit-row'>
+                  <input type="text" autoComplete='true' placeholder='Name' defaultValue={item.name} onChange={(e) =>props.handleUpdateName(e,index)} />
+                  <input type="number" autoComplete='true' placeholder='Age' defaultValue={item.age} onChange={(e) => props.handleUpdateAge(e, index)} />
+                </div>
+                <div className='edit-row'>
+                  <input type="text" autoComplete='true' placeholder='Course' defaultValue={item.course} onChange={(e) => props.handleUpdateCourse(e, index)} />
+                  <input type="text" autoComplete='true' placeholder='Batch' defaultValue={item.batch} onChange={(e) => props.handleUpdateBatch(e, index)} />
+                </div>
+              </div>
+              <Link to='/students'><button className='btn-pos'  >Cancel</button></Link>
+              <Link to='/students'><button className='btn-pos'  >Update</button></Link>
+    
             </div>
-            <div className='edit-row'>
-                <input type="text" autoComplete='true' placeholder='Course'/>
-                <input type="text" autoComplete='true' placeholder='Batch'/>
-            </div>
-        </div>
-      <button className='btn-pos'>Cancel</button>
-      <button className='btn-pos'>Update</button>
-
+          })
+        }
       </div>
     )
   }
-}
+
+export default Edit;
+
+
+
