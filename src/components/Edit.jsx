72e3d9031b6b70e3a836b7a341ff35d28ce26ecb
Tushar7567 <swain.tushar00@gmail.com>
2022-10-13 22:37:59 +0530

    Update Edit.jsx

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { useNavigate} from 'react-router-dom';
 
 const  Edit = (props) => {
+  // const result =[];
 
-  
+  // for(let i = 0; i<props.sArr.length; i++) {
+  //   if(props.in === props.sArr[i])
+  //   {result.push( <div >
+  //     <div className='edit-container'>
+  //       <div className='edit-row'>
+  //         <input type="text" autoComplete='true' placeholder='Name' defaultValue={item.name} onChange={(e) =>props.handleUpdateName(e,i)} />
+  //         <input type="number" autoComplete='true' placeholder='Age' defaultValue={item.age} onChange={(e) => props.handleUpdateAge(e, index)} />
+  //       </div>
+  //       <div className='edit-row'>
+  //         <input type="text" autoComplete='true' placeholder='Course' defaultValue={item.course} onChange={(e) => props.handleUpdateCourse(e, i)} />
+  //         <input type="text" autoComplete='true' placeholder='Batch' defaultValue={item.batch} onChange={(e) => props.handleUpdateBatch(e, i)} />
+  //       </div>
+  //     </div>
+  //     <button className='btn-pos' onClick={()=>{navigate('/students')}}  >Cancel</button>
+  //     <button className='btn-pos' onClick={()=>{navigate('/students')}} >Update</button>
+
+  //   </div>)}
+  // }
+  const num = props.i;
+  console.log(props.i);
+
+  const navigate = useNavigate();  
     return (
       <div>
         {
-          props.sArr.map((item, index) => {
+          props.sArr.slice(num,num+1).map((item, index) => {
             return <div key={`${index}`}>
               <div className='edit-container'>
                 <div className='edit-row'>
-                  <input type="text" autoComplete='true' placeholder='Name' defaultValue={item.name} onChange={(e) =>props.handleUpdateName(e,index)} />
-                  <input type="number" autoComplete='true' placeholder='Age' defaultValue={item.age} onChange={(e) => props.handleUpdateAge(e, index)} />
+                  <input type="text" autoComplete='true' placeholder='Name' defaultValue={item.name} onChange={(e) =>props.handleUpdateName(e,num)} />
+                  <input type="number" autoComplete='true' placeholder='Age' defaultValue={item.age} onChange={(e) => props.handleUpdateAge(e, num)} />
                 </div>
                 <div className='edit-row'>
-                  <input type="text" autoComplete='true' placeholder='Course' defaultValue={item.course} onChange={(e) => props.handleUpdateCourse(e, index)} />
-                  <input type="text" autoComplete='true' placeholder='Batch' defaultValue={item.batch} onChange={(e) => props.handleUpdateBatch(e, index)} />
+                  <input type="text" autoComplete='true' placeholder='Course' defaultValue={item.course} onChange={(e) => props.handleUpdateCourse(e, num)} />
+                  <input type="text" autoComplete='true' placeholder='Batch' defaultValue={item.batch} onChange={(e) => props.handleUpdateBatch(e, num)} />
                 </div>
               </div>
-              <Link to='/students'><button className='btn-pos'  >Cancel</button></Link>
-              <Link to='/students'><button className='btn-pos'  >Update</button></Link>
-    
+              <button className='btn-pos' onClick={()=>{navigate('/students')}}  >Cancel</button>
+              <button className='btn-pos' onClick={()=>{navigate('/students')}} >Update</button>
+
             </div>
           })
         }
@@ -32,4 +54,22 @@ const  Edit = (props) => {
 export default Edit;
 
 
+// {
+//   props.sArr.map((item, index) => {
+//     return <div key={`${index}`}>
+//       <div className='edit-container'>
+//         <div className='edit-row'>
+//           <input type="text" autoComplete='true' placeholder='Name' defaultValue={item.name} onChange={(e) =>props.handleUpdateName(e,index)} />
+//           <input type="number" autoComplete='true' placeholder='Age' defaultValue={item.age} onChange={(e) => props.handleUpdateAge(e, index)} />
+//         </div>
+//         <div className='edit-row'>
+//           <input type="text" autoComplete='true' placeholder='Course' defaultValue={item.course} onChange={(e) => props.handleUpdateCourse(e, index)} />
+//           <input type="text" autoComplete='true' placeholder='Batch' defaultValue={item.batch} onChange={(e) => props.handleUpdateBatch(e, index)} />
+//         </div>
+//       </div>
+//       <button className='btn-pos' onClick={()=>{navigate('/students')}}  >Cancel</button>
+//       <button className='btn-pos' onClick={()=>{navigate('/students')}} >Update</button>
 
+//     </div>
+//   })
+// }
